26ced9c37e8c136984f7b2788e6094c9be1441ac
root <root@b0a0d6aeb369>
2025-10-07 13:50:48 +0000

    Tenant sync from Kestra

---
+++ company.team/files/etl.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+from kestra import Kestra
+
+df = pd.read_csv(os.environ['DATASET'])
+total_revenue = df['total'].sum()
+Kestra.outputs({"total": total_revenue})
+
+discount_amount = float(os.environ['DISCOUNTED_AMOUNT'])
+if discount_amount > 0:
+  df['discounted_total'] = df['total'] * (1 - discount_amount)
+  df.to_csv(os.environ['FILENAME'], index=False)
